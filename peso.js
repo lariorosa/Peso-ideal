59a614161727af8dd1d06346b016695a46d40255
larissa <larideorosa@gmail.com>
2021-11-30 20:45:52 -0300

    add 1

---
+++ peso.js
@@ -0,0 +1,51 @@
+function calcularPeso() {
+    var inNome = document.getElementById("inNome");
+    var rbMasculino = document.getElementById("rbMasculino");
+    var rbFeminino = document.getElementById("rbFeminino");
+    var inAltura = document.getElementById("inAltura");
+    var outResposta = document.getElementById("outResposta");
+  
+    var nome = inNome.value;
+    var masculino = rbMasculino.checked;
+    var feminino = rbFeminino.checked;
+    var altura = Number(inAltura.value);
+  
+
+    if (nome == "" || (masculino == false && feminino == false)) {
+      alert("Por favor, informe o nome e selecione o sexo...");
+      inNome.focus();       
+      return;
+    }
+ 
+    if (altura == 0 || isNaN(altura)) {
+      alert("Por favor, informe a altura corretamente...");
+      inAltura.focus();
+      return;
+    }
+  
+
+    if (masculino) {
+      var peso = 22 * Math.pow(altura, 2);   
+    } else {
+      var peso = 21 * Math.pow(altura, 2);
+    }
+  
+ 
+    outResposta.textContent = nome + ": Seu peso ideal é " + peso.toFixed(3) + " kg";
+  }
+
+  var btResultado = document.getElementById("btCalcular");
+  btCalcular.addEventListener("click", calcularPeso);
+  
+  function limparCampos() {
+    // Limpa os conteúdos dos elementos
+    document.getElementById("inNome").value = "";
+    document.getElementById("rbMasculino").checked = false;
+    document.getElementById("rbFeminino").checked = false;
+    document.getElementById("inAltura").value = "";
+    document.getElementById("outResposta").textContent = "";
+ 
+    document.getElementById("inNome").focus();
+  }
+  var btLimpar = document.getElementById("btLimpar");
+  btLimpar.addEventListener("click", limparCampos);
